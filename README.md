fc1b0644b65480b223336ac53f0baf691066b659
apex <kieran@enbyte.dev>
2022-04-18 11:36:41 -0700

    A-Wiki becomes Anti-Airborne
    Includes changes such as the location of the articles folder and addition of a config.json file.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# A-Wiki
- A stupidly simple knowledgebase tool that operates on Markdown and ExpressJS.
+# Anti-Airborne
+### Keep your user's laptops from becoming flying saucers!
+ A simple knowledgebase tool that operates on Markdown and ExpressJS to provide just enough functionality to be useful!
 
 This system can also present embeds in popular applications such as Discord and iMessage.
 
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const matter = require('gray-matter');
 const path = require('path');
-
+const {port} = require('./config.json');
 
 app.use(express.static('public'));
 
@@ -16,31 +16,27 @@ app.set("view engine", "ejs");
 
 app.get("/:article", (req, res) => {
 
-	// read the markdown file
-	const file = matter.read(__dirname + '/views/articles/' + req.params.article + '.md');
-	
-	// use markdown-it to convert content to HTML
-	var md = require("markdown-it")();
-	let content = file.content;
-	var result = md.render(content);
-	
-	res.render("index", {
-		post: result,
-		title: file.data.title,
-		description: file.data.description,
-	});
+        // read the markdown file
+        const file = matter.read(__dirname + '/articles/' + req.params.article + '.md');
+        
+        // use markdown-it to convert content to HTML
+        var md = require("markdown-it")();
+        let content = file.content;
+        var result = md.render(content);
+        
+        res.render("index", {
+                post: result,
+                title: file.data.title,
+                description: file.data.description,
+        });
 });
 
 app.get("/", (req, res) => {
-	const posts = fs.readdirSync(__dirname + '/views/articles/').filter(file => file.endsWith('.md'));
-	res.render("wiki", {
-		posts: posts
-	});
+        const posts = fs.readdirSync(__dirname + '/articles/').filter(file => file.endsWith('.md'));
+        res.render("wiki", {
+                posts: posts
+        });
 });
 
-const options = {
-	cert: fs.readFileSync('./sslcert/fullchain.pem'),
-	key: fs.readFileSync('./sslcert/privkey.pem')
-};
-app.listen(80);
-https.createServer(options, app).listen(443);
+app.listen(port);
+console.log(`A-Wiki listening on port ${port}!`)
